3acbe5c8c47321eb09da7b75dcb5aa9a9bf05150
Çağlar Fidan <caglarfidann@gmail.com>
2014-12-25 15:18:53 +0200

    15:18 Commit

---
+++ KyuProject/src/com/gwt/kyu/client/KyuProjectEntryPoint.java
@@ -0,0 +1,12 @@
+package com.gwt.kyu.client;
+
+import com.google.gwt.core.client.EntryPoint;
+
+public class KyuProjectEntryPoint implements EntryPoint {
+	@Override
+	public void onModuleLoad() {
+		// TODO Auto-generated method stub
+//		Presenter prsntr=new SurveyPresenter(new SurveyPage());
+//		prsntr.go(RootPanel.get());
+	}
+}
